11f82f3d07d0694ab4eb755f990295b1d85e95be
juraj orisko <juraj.orisko007@gmail.com>
2024-07-18 15:34:11 +0200

    add backend/oldVersion file backend/func/files and reorganize code and files

---
+++ backend/func/FrontEnd.py
@@ -0,0 +1,17 @@
+import time
+import matplotlib
+
+class FrontEnd():
+    def __init__():
+        pass
+    @staticmethod
+    def ShowBoard(func, settings= None):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            settings = ['Accuraccy', 'Los']
+            time.sleep(1)
+            end_time = time.time()
+            print(f"Step: {result[0]} Output: {result[1]} {settings[0]}: {result[1][0]} {settings[1]}: {result[1][1]}\nTime taken: {end_time - start_time}\n--------------------------------------------")
+            return result
+        return wrapper
