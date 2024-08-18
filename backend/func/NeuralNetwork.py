ed696f77d9dc4515033ce48cb47073b0e2147a60
juraj orisko <juraj.orisko007@gmail.com>
2024-08-18 23:52:33 +0200

    combine DataPrep.py and NeuralNetwork.py and add some changes

--- backend/func/NeuralNetwork.py
+++ backend/func/NeuralNetwork.py
@@ -8,7 +8,82 @@ from keras.datasets import mnist
 from decimal import Decimal
 np.set_printoptions(threshold=np.inf)
 
-
+class DataPrep():
+    def __init__(self):
+        self.rescaling = None ##Update
+        
+    @staticmethod  
+    def list_sum(array:list) -> list:
+        sum_value = 0
+        for value in array:
+            sum_value += value
+        return sum_value
+        
+    @staticmethod  
+    def find_index_value(array:list, position:list):
+        for dim in position:
+            array = array[dim]
+        return array
+     
+    def flatten(self, input_values, flattened_list = None):
+        if flattened_list == None:
+            flattened_list = []
+        if isinstance(input_values, (list, tuple)):
+            for element in input_values:
+                self.flatten(element, flattened_list)
+        else:
+            flattened_list.append(input_values)
+        return flattened_list
+        
+    def control_shape_LT(self, obj, size, count=None):
+        if count is None:
+            count = []
+        count.append(len(obj))
+        for part in obj:
+            print(part)
+            if isinstance(part, (list, tuple)):
+                self.control_shape_LT(part, size, count)
+            else:
+                obj
+        if len(count) == len(size):
+            pass
+     
+    def find_average(self, input_values):
+        average = 0
+        value_list = self.flatten(input_values)
+        for value in value_list:
+            average += value
+        return average / len(value_list)
+    
+    def find_max(self, input_values:list):     
+        if not isinstance(input_values, (list, tuple)):
+            return input_values
+        max_value = float('-inf')
+        for element in input_values:
+            max_value_elemnet = self.find_max(element)
+            if max_value_elemnet > max_value:
+                max_value = max_value_elemnet
+        return max_value
+        
+    def find_min(self, input_values:list):     
+        if not isinstance(input_values, (list, tuple)):
+            return input_values
+        min_value = float('inf')
+        for element in input_values:
+            min_value_elemnet = self.find_min(element)
+            if min_value_elemnet < min_value:
+                min_value = min_value_elemnet
+        return min_value
+        
+    def find_shape_LT(self, array, size=None) -> list:  # Add self parameter
+        if size is None:
+            size = []
+        if isinstance(array, (list, tuple)):
+            size.append(len(array))
+            if isinstance(array[0], (list, tuple)):  # Correct type checking
+                self.find_shape_LT(array[0], size)
+        return size
+    
 class Input():      
     """
     This class contain Inputs data and operations for processing input.
@@ -121,6 +196,7 @@ class Layer(Input, Output, WeightsFunctions):
     NEURONS [ ][ ]\n
     Weights [ ][ ][0]\n
     Bias [ ][ ][1]
+
     EXAMPLES
     --------
     First you need to create a model.
@@ -132,14 +208,7 @@ class Layer(Input, Output, WeightsFunctions):
     def __init__(self):
         super().__init__()
         self.layer_data_shape = None
-    """    
-    def settings(self, *args, **kwargs):
-        if kwargs:
-            directory_looker(kwargs)
-        def directory_looker(dictionary:dict):
-            for index, obj in enumerate(dictionary):
-                print(index, obj)
-    """
+
     def random_weights(self, input_values):
         return np.random.uniform(-(self.randomness_coefficient), self.randomness_coefficient, input_values).astype(np.float64) 
         
@@ -230,23 +299,54 @@ class AdditionalFunctions():
             end_time = time.time()
         return wrapper
 
-class Settings():
+class Settings(): #REMAKE DOCSTRING
+    """
+    PURPOSE
+    ------
+    Class used for storing setting used in models.
+
+    Is it used for adding layers to the model, and has lot of functions for processing data in layers.
+    
+    TYPES OF ACTIVATIONS:
+    ---------------------
+    Adam - 
+    SGD - 
+
+    TYPES of LOSSES:
+    ----------------
+    MSE - 
+    EvalueatingCost - 
+    CrossEntropyLoss - 
+
+    EXAMPLE
+    --------
+    >>> your_model.settings("Adam",
+                            "MSE",
+                            "precision"                 
+        )
+    """
     def __init__(self):
         self.activation_opt = {
-            True: True
-        }
-        self.loss_type = None
-        self.loss_opt = {
-            True: True
-        }
-        self.accuracy_type = None
-        self.accuracy_opt = {
-            True: True
+            "adam": self.Adam,
+            "sgd": self.SGD
         }
         self.activation_type = None
+        self.loss_opt = {
+            "mse": self.MeanSquaredError
+        }
+        self.loss_type = None
+        self.accuracy_type = None
+        self.accuracy_opt = {
+            "precision": self.precision
+        }
 
-    def settings(self, activation, loss, accuracy): #WORK ON THIS PLEASE
-        pass
+    def settings(self, activation_type:str=None, loss_type:str=None, accuracy_type:str=None): # STILL WORK ON 
+        try:
+            self.activation_type = self.accuracy_opt[accuracy_type.lower()]
+            self.loss_type = self.loss_opt[loss_type.lower()]
+            self.activation_type = self.accuracy_opt[activation_type.lower()]
+        except:
+            pass
 
 class Activations():
     def __init__(self): 
@@ -348,9 +448,6 @@ class NeuralNetwork(Layer, CostFunctions, AdditionalFunctions,  Settings, Activa
             for layer in self.layers:
                 file.write({layer})
         # [[weights, bias], settinggs, ]
-    def settings(self, activation_type=None, loss_type=None, accuracy_type=None): # WORK ON 
-        pass
-        
     
     def vector_multiply(self, input_values, neuron): #REWORK docstring and PURPOSE!!!
         """
@@ -435,6 +532,7 @@ class NeuralNetwork(Layer, CostFunctions, AdditionalFunctions,  Settings, Activa
             self.layers.append(func[0])
             self.activation_layers.append(func[1])
         self.check_activation()
+        
 class linearRegression(Input, Output):
     def __init__(self):
         self.slopes = None #[[m1, b1], ...]
--- backend/func/test.py
+++ backend/func/test.py
@@ -2,5 +2,5 @@ import numpy as np
 list = [1,2,3,4,5]
 array = np.array([])
 arra2 = np.array(array)
-
-print(array)
+string = "HANSHFSA"
+print(string.lower())
